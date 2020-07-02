4c8ddebeedd1726d791784199593cf57f4bc0ae2
amirazz <zranamira9@gmail.com>
2020-07-02 17:54:06 +0200

    Vidly Project with Form and Register final

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-const Input = ({ name, label, value, error, onChange }) => {
+const Input = ({ type, name, label, value, error, onChange }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
@@ -9,7 +9,7 @@ const Input = ({ name, label, value, error, onChange }) => {
         onChange={onChange}
         name={name}
         id={name}
-        type="text"
+        type={type}
         className="form-control"
       />
       {error && <div className="alert alert-danger">{error}</div>}
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,65 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
-import Input from "./common/input";
-class LoginForm extends Component {
+import Form from "./common/form";
+
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
-    errors: {},
-  };
-  schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
+    data: { username: "", password: "" },
+    errors: {}
   };
 
-  validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, { abortEarly: false });
-    if (!result.error) return null;
-    const errors = {}
-    for (let item of result.error.details)
-      errors[item.path[0]] = item.message;
-    return errors;
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
   };
-  handelChange = ({ currentTarget: input }) => {
-    console.log({ currentTarget: input });
-    console.log(input.name, input.value);
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account });
-  };
-  handelSubmit = (e) => {
-    e.preventDefault();
-    //console.log(e.preventDefault());
-    const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors: errors || {} });
-    //if (errors) return;
+
+  doSubmit = () => {
+    // Call the server
     console.log("Submitted");
   };
 
   render() {
-    const { account, errors } = this.state;
     return (
       <div>
-        <h1> Login</h1>
-        <form onSubmit={this.handelSubmit}>
-          <Input
-            name="username"
-            value={account.value}
-            onChange={this.handelChange}
-            //name={this.username}
-            label={"UserName"}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={account.value}
-            onChange={this.handelChange}
-            //name={this.password}
-            label={"Password"}
-            error={errors.password}
-          />
-
-          <button className="btn bnt-primary">Login</button>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
@@ -67,4 +37,3 @@ class LoginForm extends Component {
 }
 
 export default LoginForm;
-
